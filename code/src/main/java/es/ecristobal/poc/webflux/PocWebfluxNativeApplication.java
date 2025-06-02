1cc5f0871ef5a248ac4546d5dfb575ad3eb14f00
Esteban Cristóbal <ecristobalr@gmail.com>
2025-06-02 16:24:28 +0200

    feat(code): Removed OpenTelemetry logging

--- code/src/main/java/es/ecristobal/poc/webflux/PocWebfluxNativeApplication.java
+++ code/src/main/java/es/ecristobal/poc/webflux/PocWebfluxNativeApplication.java
@@ -1,9 +1,6 @@
 package es.ecristobal.poc.webflux;
 
-import io.opentelemetry.api.OpenTelemetry;
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 import static org.springframework.boot.SpringApplication.run;
 import static reactor.core.publisher.Hooks.enableAutomaticContextPropagation;
@@ -14,11 +11,6 @@ public class PocWebfluxNativeApplication {
     public static void main(String[] args) {
         enableAutomaticContextPropagation();
         run(PocWebfluxNativeApplication.class, args);
-    }
-
-    @Bean
-    InitializingBean openTelemetryAppenderInitializer(final OpenTelemetry openTelemetry) {
-        return new OpenTelemetryAppenderInitializer(openTelemetry);
     }
 
 }
