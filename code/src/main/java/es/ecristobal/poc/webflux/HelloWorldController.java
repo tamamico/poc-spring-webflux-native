1a4f8612cf90687e60ab1e9a02bf8ab59d8d7b10
Esteban Cristóbal <ecristobalr@gmail.com>
2025-09-21 10:37:18 +0200

    style(code): Fixed SonarQube issue

--- code/src/main/java/es/ecristobal/poc/webflux/HelloWorldController.java
+++ code/src/main/java/es/ecristobal/poc/webflux/HelloWorldController.java
@@ -2,6 +2,7 @@ package es.ecristobal.poc.webflux;
 
 import io.opentelemetry.instrumentation.annotations.WithSpan;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +14,7 @@ class HelloWorldController {
     private static final String GREETING_TEMPLATE = "Greeting %s!";
 
     @WithSpan
-    @GetMapping("/greet/{name}")
+    @GetMapping(path = "/greet/{name}", produces = MediaType.TEXT_PLAIN_VALUE)
     String greet(final @PathVariable String name) {
         log.atInfo()
            .setMessage("Greeting {}")
